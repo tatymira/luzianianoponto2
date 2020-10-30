8cd13b64d5fb8a0bcb9d475b253498a6cff70f6f
Tatyana Mira <tatyanamira132@gmail.com>
2020-10-30 00:30:54 -0300

    Classes

---
+++ Domain/Classes/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Classes
+{
+    public class Empresa
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Cnpj { get; set; }
+
+        public virtual string RazaoSocial { get; set; }
+
+        public virtual string Nome { get; set; }
+
+        public virtual string Email { get; set; }
+
+        public virtual string Senha { get; set; }
+    }
+}
---
+++ Domain/Classes/Linha.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Domain.Classes
+{
+    public class Linha
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Nome { get; set; }
+
+        public virtual decimal Tarifa { get; set; }
+
+        public virtual string Origem { get; set; }
+
+        public virtual string Destino { get; set; }
+
+        public virtual int Numero { get; set; }
+
+        public virtual Empresa Empresa { get; set; }
+
+        public virtual string LinhaMap { get; set; }
+    }
+}
