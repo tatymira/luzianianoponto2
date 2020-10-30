4f972f52edbc09803f91f8e7917584c1d5ea84b6
Tatyana Mira <tatyanamira132@gmail.com>
2020-10-29 23:27:21 -0300

    Arquitetura do projeto

---
+++ Api/Controllers/LinhaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[linha]")]
+    public class LinhaController : ControllerBase
+    {
+
+        private readonly ILogger<LinhaController> _logger;
+
+        public LinhaController(ILogger<LinhaController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55)
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace LuzianiaNoPonto2
+namespace Api
 {
     public class Program
     {
