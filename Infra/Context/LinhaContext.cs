01d495df0d4e9e7ba047c0bd3c87abb89627a161
Tatyana Mira <tatyanamira132@gmail.com>
2020-11-04 23:15:34 -0300

    Context Entity Framework

---
+++ Infra/Context/LinhaContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Classes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.Context
+{
+    public class LinhaContext : DbContext
+    {
+        public DbSet<Linha> Linha { get; set; } 
+    }
+}
